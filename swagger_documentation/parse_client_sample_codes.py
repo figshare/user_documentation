ab321c6f0927a6922faaf3c900f12628d8aba959
Tudor Amariei <tudor.amariei@figshare.com>
2021-12-08 14:22:57 +0200

    ⬆️ Update repo to Python 3
    - run 2to3 on the code
    - change dependencies to become Py3 Compatible
    - format files with Black
    - makefile changes
      - add black formatting command
      - change configure to install in commands
    - clean-up names of methods and variables
    - clean-up README.md

--- swagger_documentation/parse_client_sample_codes.py
+++ swagger_documentation/parse_client_sample_codes.py
@@ -1,28 +1,28 @@
-from pprint import pprint
 import json
 
 from lxml import html
 from lxml.etree import tostring
 
 
-def getApiOperationID(name):
-    name = name[:len(name)-2]
-    name = name[name.rfind('-')+1:]
+def get_api_operation_id(name):
+    name = name[: len(name) - 2]
+    name = name[name.rfind("-") + 1 :]
     return name
+
 
 sample_code = {}
 
 with open("html2/index.html", "r") as f:
     page = f.read()
 tree = html.fromstring(page)
-sections = tree.get_element_by_id('sections')
-for section in sections.findall('section'):
-    for div in section.findall('div/article'):
-        operationId = getApiOperationID(div.attrib["id"])
-        nav_tab = div.find_class('nav-tabs-examples')[0]
-        tab_content = div.find_class('tab-content')[0]
-        sample_code[operationId] = (tostring(nav_tab), tostring(tab_content))
+sections = tree.get_element_by_id("sections")
+for section in sections.findall("section"):
+    for div in section.findall("div/article"):
+        operationId = get_api_operation_id(div.attrib["id"])
+        nav_tab = div.find_class("nav-tabs-examples")[0]
+        tab_content = div.find_class("tab-content")[0]
+        sample_code[operationId] = (tostring(nav_tab).decode(), tostring(tab_content).decode())
 
 
-with open('sample_code.json', 'w') as outfile:
+with open("sample_code.json", "w") as outfile:
     json.dump(sample_code, outfile)
